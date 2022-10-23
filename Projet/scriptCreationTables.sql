063698554b1616829034046e86924b2ea163962d
Alex L <alexandre.letellier09@gmail.com>
2022-10-23 21:54:13 +0200

    fix: maintenant ça marche pour de vrai

--- Projet/scriptCreationTables.sql
+++ Projet/scriptCreationTables.sql
@@ -1,12 +1,13 @@
-DROP TABLE Administrateur;
 
-DROP TABLE Utilisateur;
+DROP TABLE Utilisateur CASCADE;
 
-DROP TABLE Demande_Question;
+DROP TABLE Administrateur CASCADE;
 
-DROP TABLE Question;
+DROP TABLE Demande_Question CASCADE;
 
-DROP TABLE Section;
+DROP TABLE Question CASCADE;
+
+DROP TABLE Section CASCADE;
 
 CREATE TABLE Utilisateur (
     idUtilisateur serial,
@@ -28,15 +29,17 @@ CREATE TABLE Demande_Question (
     intitule text NOT NULL,
     estValide boolean NOT NULL DEFAULT FALSE,
     idUtilisateur serial, -- nom de l'organisateur de la question
-    CONSTRAINT pk_Question PRIMARY KEY (idQuestion),
-    CONSTRAINT fk_organisateur FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)
+    CONSTRAINT pk_demande_question PRIMARY KEY (idQuestion),
+    CONSTRAINT fk_demande_question_organisateur FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)
 );
 
 CREATE TABLE Question (
     dateDebutRedaction timestamp NOT NULL,
     dateFinRedaction timestamp NOT NULL,
     dateOuvertureVotes timestamp NOT NULL,
-    dateFermetureVotes timestamp NOT NULL
+    dateFermetureVotes timestamp NOT NULL,
+    CONSTRAINT pk_question PRIMARY KEY (idQuestion),
+    CONSTRAINT fk_question_organisateur FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)
 )
 INHERITS (
     Demande_Question
@@ -46,7 +49,7 @@ CREATE TABLE Section (
     idSection serial,
     idQuestion serial NOT NULL,
     nomSection varchar(50) NOT NULL,
-    CONSTRAINT pk_Section PRIMARY KEY (idSection),
-    CONSTRAINT fk_Section FOREIGN KEY (idQuestion) REFERENCES Question (idQuestion)
+    CONSTRAINT pk_section PRIMARY KEY (idSection),
+    CONSTRAINT fk_section_question FOREIGN KEY (idQuestion) REFERENCES Question (idQuestion)
 );
 
