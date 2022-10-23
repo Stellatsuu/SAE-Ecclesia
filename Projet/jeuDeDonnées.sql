7e190b4244c72ba1be5fb8c13291f1ea02dbc52e
Alex L <alexandre.letellier09@gmail.com>
2022-10-23 21:40:08 +0200

    Poser une question - Mise à jour des tables

--- Projet/jeuDeDonnées.sql
+++ Projet/jeuDeDonnées.sql
@@ -8,5 +8,5 @@ INSERT INTO Utilisateur(nom, prenom, motDePasse) VALUES('Uchiha', 'Itachi', '123
 -- Administrateurs
 
 -- Questions
-INSERT INTO Question(question, intitule, idUtilisateur) VALUES('Doit-on attaquer l''akatsuki ?', 'L''akatsuki est de plus en plus menaçante, nous avons répéré leur repaire, devrions-nous lancer un assaut en association avec le village caché du sable, Suna ? Et sous quelle forme ?', 1);
-INSERT INTO Question(question, intitule, idUtilisateur, estValide) VALUES('Comment devrions-nous organiser notre sortie pour manger des ramens chez Ichiraku ?', 'Ichiraku a présenté de nouvelles recettes de ramens, discutons de notre organisation.', 1, TRUE);
+INSERT INTO Demande_Question(titre, intitule, idUtilisateur) VALUES('Doit-on attaquer l''akatsuki ?', 'L''akatsuki est de plus en plus menaçante, nous avons répéré leur repaire, devrions-nous lancer un assaut en association avec le village caché du sable, Suna ? Et sous quelle forme ?', 1);
+INSERT INTO Demande_Question(titre, intitule, idUtilisateur, estValide) VALUES('Comment devrions-nous organiser notre sortie pour manger des ramens chez Ichiraku ?', 'Ichiraku a présenté de nouvelles recettes de ramens, discutons de notre organisation.', 1, TRUE);
--- Projet/scriptCreationTables.sql
+++ Projet/scriptCreationTables.sql
@@ -1,27 +1,52 @@
-DROP TABLE Question;
 DROP TABLE Administrateur;
+
 DROP TABLE Utilisateur;
 
-CREATE TABLE Utilisateur(
-    idUtilisateur SERIAL,
-    nom VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
-    motDePasse VARCHAR(128) NOT NULL,
-    CONSTRAINT pk_Utilisateur PRIMARY KEY(idUtilisateur)
+DROP TABLE Demande_Question;
+
+DROP TABLE Question;
+
+DROP TABLE Section;
+
+CREATE TABLE Utilisateur (
+    idUtilisateur serial,
+    nom varchar(50) NOT NULL,
+    prenom varchar(50) NOT NULL,
+    motDePasse varchar(128) NOT NULL,
+    CONSTRAINT pk_Utilisateur PRIMARY KEY (idUtilisateur)
 );
 
-CREATE TABLE Administrateur(
-    idUtilisateur SERIAL,
-    CONSTRAINT pk_Administrateur PRIMARY KEY(idUtilisateur),
-    CONSTRAINT fk_Administrateur FOREIGN KEY(idUtilisateur) REFERENCES Utilisateur(idUtilisateur)
+CREATE TABLE Administrateur (
+    idUtilisateur serial,
+    CONSTRAINT pk_Administrateur PRIMARY KEY (idUtilisateur),
+    CONSTRAINT fk_Administrateur FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)
 );
 
-CREATE TABLE Question(
-    idQuestion SERIAL,
-    question TEXT NOT NULL,
-    intitule TEXT NOT NULL,
-    estValide BOOLEAN NOT NULL DEFAULT FALSE,
-    idUtilisateur SERIAL, -- nom de l'organisateur de la question
-    CONSTRAINT pk_Question PRIMARY KEY(idQuestion),
-    CONSTRAINT fk_organisateur FOREIGN KEY(idUtilisateur) REFERENCES Utilisateur(idUtilisateur)
+CREATE TABLE Demande_Question (
+    idQuestion serial,
+    titre text NOT NULL,
+    intitule text NOT NULL,
+    estValide boolean NOT NULL DEFAULT FALSE,
+    idUtilisateur serial, -- nom de l'organisateur de la question
+    CONSTRAINT pk_Question PRIMARY KEY (idQuestion),
+    CONSTRAINT fk_organisateur FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)
 );
+
+CREATE TABLE Question (
+    dateDebutRedaction timestamp NOT NULL,
+    dateFinRedaction timestamp NOT NULL,
+    dateOuvertureVotes timestamp NOT NULL,
+    dateFermetureVotes timestamp NOT NULL
+)
+INHERITS (
+    Demande_Question
+);
+
+CREATE TABLE Section (
+    idSection serial,
+    idQuestion serial NOT NULL,
+    nomSection varchar(50) NOT NULL,
+    CONSTRAINT pk_Section PRIMARY KEY (idSection),
+    CONSTRAINT fk_Section FOREIGN KEY (idQuestion) REFERENCES Question (idQuestion)
+);
+
