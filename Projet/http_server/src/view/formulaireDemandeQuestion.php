fafebd340b2d0d39ea97c159c1ba8944e1950159
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-10-27 23:32:30 +0200

    début clean css

--- Projet/http_server/src/view/formulaireDemandeQuestion.php
+++ Projet/http_server/src/view/formulaireDemandeQuestion.php
@@ -1,15 +1,14 @@
-<form method="post" action="../../../web/frontController.php?action=demanderCreationQuestion">
-             <h1>Proposer une question</h1>
-                <fieldset>
-                    <label for="titre_id">Question :</label>
-                     <textarea rows=6 cols=50 id="titre_id" placeholder="Écrivez votre question ici" name="titre" required></textarea>
+<form method="post" action="../../../web/frontController.php?action=demanderCreationQuestion" class="panel">
+    <h1>Proposer une question</h1>
+    <fieldset>
+        <label for="titre_id">Question :</label>
+         <textarea rows=6 cols=50 id="titre_id" placeholder="Écrivez votre question ici" name="titre" required></textarea>
 
-                     <label for="intitule_id">Intitulé :</label>
-                     <textarea rows=6 cols=50 id="intitule_id" placeholder="Écrivez les détails de votre question ici, la raison de cette demande, etc" name="intitule" required></textarea>
+         <label for="intitule_id">Intitulé :</label>
+         <textarea rows=6 cols=50 id="intitule_id" placeholder="Écrivez les détails de votre question ici, la raison de cette demande, etc" name="intitule" required></textarea>
+    </fieldset>
 
-                </fieldset>
-
-                    <label for="idUtilisateur_id"></label>
-                    <input type="number" name="idUtilisateur" id="idUtilisateur_id" required>
-                    <input type="submit" value="Valider">
+        <label for="idUtilisateur_id"></label>
+        <input type="number" name="idUtilisateur" id="idUtilisateur_id" required>
+        <input type="submit" value="Valider">
 </form>
