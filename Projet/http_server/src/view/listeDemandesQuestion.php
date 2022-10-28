1d9ce9ec0e6fa60ec985f6a6008278227644f7f3
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-10-28 12:57:13 +0200

    preotection listeDemandesQuestion

--- Projet/http_server/src/view/listeDemandesQuestion.php
+++ Projet/http_server/src/view/listeDemandesQuestion.php
@@ -11,14 +11,14 @@
         foreach ($demandes as $q) {
             $i++;
             echo "<div class='demandeQuestion'><div class='boite' style='--order: " . $i . "'>";
-            echo ("<h2>" . $q->getTitre() . "</h2>");
-            echo ("<p>" . $q->getIntitule() . "</p>");
-            echo ("<p>- " . $q->getOrganisateur()->getPrenom() . " " . $q->getOrganisateur()->getNom()) . "</p>";
+            echo ("<h2>" . htmlspecialchars($q->getTitre()) . "</h2>");
+            echo ("<p>" . htmlspecialchars($q->getIntitule()) . "</p>");
+            echo ("<p>- " . htmlspecialchars($q->getOrganisateur()->getPrenom()) . " " . htmlspecialchars(strtoupper($q->getOrganisateur()->getNom()))) . "</p>";
 
             echo "</div>";
             echo "<div class='boite'>";
-            echo ("<a class='button refuserBtn' href='frontController.php?action=refuserDemandeQuestion&idQuestion=" . $q->getIdQuestion() . "'>Refuser</a>");
-            echo ("<a class='button validerBtn' href='frontController.php?action=accepterDemandeQuestion&idQuestion=" . $q->getIdQuestion() . "'>Valider</a>");
+            echo ("<a class='button refuserBtn' href='frontController.php?action=refuserDemandeQuestion&idQuestion=" . urlencode($q->getIdQuestion()) . "'>Refuser</a>");
+            echo ("<a class='button validerBtn' href='frontController.php?action=accepterDemandeQuestion&idQuestion=" . urlencode($q->getIdQuestion()) . "'>Valider</a>");
             echo "</div></div>";
         }
     ?>
