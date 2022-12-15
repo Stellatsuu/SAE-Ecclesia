9e1c03e2f8699e8ed992e48a0e11514be71399f5
Renucci Stella-Maria <stella-maria.renucci@etu.umontpellier.fr>
2022-12-15 14:53:12 +0100

    Avancements CSS

--- Projet/http_server/src/view/afficherProfil.php
+++ Projet/http_server/src/view/afficherProfil.php
@@ -18,11 +18,9 @@ $pfp = <<<html
 
     <div id="contenuProfil">
         <div id="PPusername">
-            <a class="modal-open" href="#modalModifierPFP" id="PP">
-                <?= $pfp ?>
-            </a>
+            <p id="PP"><?= $pfp ?></p>
             <p><?php echo $utilisateur->getUsername(); ?></p>
-            <img src='./assets/images/image-icon.svg' id="editPP" alt="editImageIcon"/>
+            <a class="modal-open" href="#modalModifierPFP"><img src='./assets/images/image-icon.svg' id="editPP" alt="editImageIcon"/></a>
         </div>
 
         <div id="informations">
@@ -46,25 +44,25 @@ $pfp = <<<html
 
 <div class="modal" id="modalModifierPFP">
     <div class="modalContent panel">
-        <h2>Modifier ma photo de profil</h2>
-
         <form enctype="multipart/form-data" action="frontController.php?controller=utilisateur&action=modifierPFP" method="post" enctype="multipart/form-data">
+            <h2>Modifier ma photo de profil</h2>
+
             <input type="hidden" name="MAX_FILE_SIZE" value="8000000" />
             <input type="file" name="userfile" accept="image/png" required>
             <input type="submit" value="Modifier">
-        </form>
 
-        <a href="#" class="modal-close">
-            <img src="assets/images/close-icon.svg" alt="bouton fermer">
-        </a>
+            <a href="#" class="modal-close">
+                <img src="assets/images/close-icon.svg" alt="bouton fermer">
+            </a>
+        </form>
     </div>
 </div>
 
 <div class="modal" id="modalModifierNomPrenom">
     <div class="modalContent panel">
-        <h2>Modifier mon nom/prénom</h2>
-
         <form action="frontController.php?controller=utilisateur&action=modifierNomPrenom" method="post">
+            <h2>Modifier mon nom/prénom</h2>
+
             <label>Nom</label>
             <input type="text" name="nom" value="<?php echo $utilisateur->getNom(); ?>" required>
 
@@ -72,36 +70,36 @@ $pfp = <<<html
             <input type="text" name="prenom" value="<?php echo $utilisateur->getPrenom(); ?>" required>
 
             <input type="submit" value="Modifier">
-        </form>
 
-        <a href="#" class="modal-close">
-            <img src="assets/images/close-icon.svg" alt="bouton fermer">
-        </a>
+            <a href="#" class="modal-close">
+                <img src="assets/images/close-icon.svg" alt="bouton fermer">
+            </a>
+        </form>
     </div>
 </div>
 
 <div class="modal" id="modalModifierEmail">
     <div class="modalContent panel">
-        <h2>Modifier mon adresse mail</h2>
-
         <form action="frontController.php?controller=utilisateur&action=modifierEmail" method="post">
+            <h2>Modifier mon adresse mail</h2>
+
             <label>Adresse mail</label>
             <input type="email" name="email" value="<?php echo $utilisateur->getEmail(); ?>" required>
 
             <input type="submit" value="Modifier">
-        </form>
 
-        <a href="#" class="modal-close">
-            <img src="assets/images/close-icon.svg" alt="bouton fermer">
-        </a>
+            <a href="#" class="modal-close">
+                <img src="assets/images/close-icon.svg" alt="bouton fermer">
+            </a>
+        </form>
     </div>
 </div>
 
 <div class="modal" id="modalModifierMDP">
     <div class="modalContent panel">
-        <h2>Modifier mon mot de passe</h2>
-
         <form action="frontController.php?controller=utilisateur&action=modifierMDP" method="post">
+            <h2>Modifier mon mot de passe</h2>
+
             <label>Mot de passe actuel</label>
             <input type="password" name="ancienMDP" required>
 
@@ -112,21 +110,21 @@ $pfp = <<<html
             <input type="password" name="nouveauMDP2" required>
 
             <input type="submit" value="Modifier">
+
+            <a href="#" class="modal-close">
+                <img src="assets/images/close-icon.svg" alt="bouton fermer">
+            </a>
         </form>
 
-        <a href="#" class="modal-close">
-            <img src="assets/images/close-icon.svg" alt="bouton fermer">
-        </a>
     </div>
 </div>
 
 <div class="modal" id="modalSupprimerCompte">
     <div class="modalContent panel">
-        <h2>Supprimer mon compte</h2>
-
-        <p style="color: red;">Attention, cette action est irréversible !</p>
-
         <form action="frontController.php?controller=utilisateur&action=supprimerCompte" method="post" autocomplete="off">
+            <h2>Supprimer mon compte</h2>
+
+            <p style="color: red;">Attention, cette action est irréversible !</p>
             <input autocomplete="false" name="hidden" type="text" style="display:none;"> <!-- Pour éviter que le navigateur propose de remplir le formulaire -->
 
             <label>Entrez votre mot de passe</label>
@@ -136,10 +134,10 @@ $pfp = <<<html
             <label>Je comprends que cette action est irréversible</label>
 
             <input type="submit" value="Supprimer mon compte">
-        </form>
 
-        <a href="#" class="modal-close">
-            <img src="assets/images/close-icon.svg" alt="bouton fermer">
-        </a>
+            <a href="#" class="modal-close">
+                <img src="assets/images/close-icon.svg" alt="bouton fermer">
+            </a>
+        </form>
     </div>
 </div>
