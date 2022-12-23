27c200b3de00f1808f6b6e6ce66e9b7cdcaf3a95
Renucci Stella-Maria <stella-maria.renucci@etu.umontpellier.fr>
2022-12-23 21:08:03 +0100

    Suppression de l'onglet parametres inutile pour le moment

--- Projet/http_server/src/view/view.php
+++ Projet/http_server/src/view/view.php
@@ -17,7 +17,6 @@ if ($estConnecte) {
     <li><a href="frontController.php?controller=utilisateur&action=afficherProfil">Mon compte</a></li>
     <li><a href="frontController.php?controller=question&action=listerMesQuestions">Mes questions</a></li>
     $lienDemandes
-    <li><a href="frontController.php?controller=utilisateur&action=afficherParametres">Paramètres</a></li>
     <li><a href="frontController.php?controller=utilisateur&action=seDeconnecter">Se déconnecter</a></li>
     html;
 
