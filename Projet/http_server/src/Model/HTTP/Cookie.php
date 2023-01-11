a78b81c1217ca15c7cf369b4670406571fa30d5d
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2023-01-11 22:24:31 +0100

    re cookie

---
+++ Projet/http_server/src/Model/HTTP/Cookie.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\SAE\Model\HTTP;
+
+class Cookie
+{
+
+    public static function enregistrer(string $cle, mixed $valeur, ?int $dureeExpiration = null): void
+    {
+        $serializedValeur = serialize($valeur);
+
+        if ($dureeExpiration == null) {
+            $expiration = 0;
+        } else {
+            $expiration = time() + $dureeExpiration;
+        }
+
+        setcookie($cle, $serializedValeur, $expiration);
+    }
+
+    public static function lire($cle): mixed
+    {
+        if (isset($_COOKIE[$cle])) {
+            return unserialize($_COOKIE[$cle]);
+        }
+        return null;
+    }
+
+    public static function contient($cle): bool
+    {
+        return isset($_COOKIE[$cle]);
+    }
+
+    public static function supprimer($cle): void
+    {
+        setcookie($cle, '', time() - 3600);
+    }
+}
